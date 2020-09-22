3936f94bef6f96772c9f6c447394a7bb166b3fb3
阿鑫 <8101943+qqxinaaaaa@user.noreply.gitee.com>
2020-09-22 18:19:59 +0800

    deploy

---
+++ precache-manifest.01648880dec56f3ced64ee64c0f6b892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab69e04ba2e0f85b197",
+    "url": "/xinxin-website/css/app.c57908bd.css"
+  },
+  {
+    "revision": "b0f7741164541851696cf276161f44c5",
+    "url": "/xinxin-website/index.html"
+  },
+  {
+    "revision": "7ab69e04ba2e0f85b197",
+    "url": "/xinxin-website/js/app.7a980683.js"
+  },
+  {
+    "revision": "890d8b2264d7de528409",
+    "url": "/xinxin-website/js/chunk-vendors.ec3144da.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/xinxin-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xinxin-website/robots.txt"
+  }
+]);
